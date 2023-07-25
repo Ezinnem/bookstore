06510396273be0e3f88f201675b90d73991b0601
martina <you@example.com>
2023-07-25 21:27:48 +0100

    The utils, controllers and models folders added

---
+++ pkg/config/app.go
@@ -0,0 +1,22 @@
+package config
+//The aim of this file is the handle and return the database and help other files to interact with the database
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db * gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "root:123456789/bookstore?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB{
+	return db
+}
